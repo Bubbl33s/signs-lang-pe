dbd3492a683ea42ed2935ce07be20b105b9cc302
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-10 18:35:04 -0500

    fix: deleted some archs

--- src/routes/productRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { ProductController } from "../controllers/productController";
-
-const router = Router();
-const PREFIX = "/products";
-
-router.get(PREFIX, ProductController.getProducts);
-router.get(`${PREFIX}/id/:id`, ProductController.getProductById);
-export default router;
