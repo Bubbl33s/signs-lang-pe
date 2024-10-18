7be773938bfd9d6ff8f3ba6c150a20bdc52fb39b
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-18 08:16:36 -0500

    fix: fix dist

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express, { Request, Response } from "express";
-import productRoutes from "./routes/productRoutes";
 
 const app = express();
-const port = 3000;
-
-app.use("/api", productRoutes);
+const port = 9090;
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Hello, world!");
