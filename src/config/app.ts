ee8fbd25c442228d1467106643128320062a22a8
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-05 12:30:38 -0500

    feat: setup app with helmet, cors, compression and morgan

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,7 @@
 import express from "express";
-import setupExpress from "./config/express";
-import path from "path";
+import setupExpress from "./express";
 
 const app = express();
 setupExpress(app);
-// Servir la carpeta uploads de forma estática
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 export default app;
