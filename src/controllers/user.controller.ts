43bd0da48913d8e4c222079bc182b27e1fb36e72
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-06 12:48:09 -0500

    feat: add user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,67 @@
+import { Request, Response, NextFunction } from "express";
+import { UserService } from "../services/user.service";
+import { CreateUser, UpdateUser } from "../types";
+
+export class UserController {
+  static async getUsers(_: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await UserService.getUsers();
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getUserById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+
+      const user = await UserService.getUserById(userId);
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async createUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const user = req.body as CreateUser;
+
+      const newUser = await UserService.createUser(user);
+
+      res.json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async updateUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+      const user = req.body as UpdateUser;
+
+      const updatedUser = await UserService.updateUser(userId, user);
+
+      res.json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params;
+
+      await UserService.deleteUser(userId);
+
+      res.json({ message: "User deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
