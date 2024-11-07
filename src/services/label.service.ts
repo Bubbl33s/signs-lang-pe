f7aeafb3aff540398417a9993d2a094a8413f8c9
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-07 12:37:12 -0500

    feat: add label service

---
+++ src/services/label.service.ts
@@ -0,0 +1,61 @@
+import { Label } from "../models";
+
+export default class LabelService {
+  static async getLabels() {
+    return Label.find();
+  }
+
+  static async getLabelById(labelId: string) {
+    return Label.findById(labelId);
+  }
+
+  static async getLabelByName(name: string) {
+    return Label.findOne({ name });
+  }
+
+  static async createLabel(name: string) {
+    const labelWithSameName = await Label.findOne({ name });
+
+    if (labelWithSameName) {
+      throw new Error("Etiqueta ya existe");
+    }
+
+    return Label.create({ name });
+  }
+
+  static async updateLabel(labelId: string, name: string) {
+    const labelExists = await this.getLabelById(labelId);
+
+    if (!labelExists) {
+      throw new Error("Etiqueta no encontrada");
+    }
+
+    const labelWithSameName = await Label.findOne({ name });
+
+    if (labelWithSameName) {
+      throw new Error("Etiqueta ya existe");
+    }
+
+    return Label.findByIdAndUpdate(labelId, { name }, { new: true });
+  }
+
+  static async verifyLabel(labelId: string) {
+    const labelExists = await this.getLabelById(labelId);
+
+    if (!labelExists) {
+      throw new Error("Etiqueta no encontrada");
+    }
+
+    return Label.findByIdAndUpdate(labelId, { verified: true }, { new: true });
+  }
+
+  static async deleteLabel(labelId: string) {
+    const labelExists = await this.getLabelById(labelId);
+
+    if (!labelExists) {
+      throw new Error("Etiqueta no encontrada");
+    }
+
+    return Label.findByIdAndDelete(labelId);
+  }
+}
